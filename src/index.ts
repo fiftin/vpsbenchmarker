601784cd6907804ecbcdf6a42192b1149c7fd7a8
Denis <denguk@gmail.com>
2018-10-12 17:42:01 +0500

    feat: parallel execution of benchmarks

--- src/index.ts
+++ src/index.ts
@@ -93,8 +93,8 @@ const logger = console;
 (async () => {
     const providerInfo = config.providers[argv.provider];
     const provider = new ProviderFactory().createProvider(argv.provider, providerInfo.settings);
-    for (const [serverId, serverBenchmarks] of getProviderBenchmarks(argv.provider)) {
-
+    const providerBenchmarks = getProviderBenchmarks(argv.provider)[Symbol.iterator]();
+    await Promise.all([...providerBenchmarks].map(async ([serverId, serverBenchmarks]) => {
         const benchmarker = new Benchmarker(provider, serverBenchmarks);
         const serverResults = await benchmarker.start({
             id: serverId,
@@ -107,7 +107,23 @@ const logger = console;
         });
 
         await storage.storeServerResults(serverId, serverResults);
-    }
+    }));
+
+    // for (const [serverId, serverBenchmarks] of getProviderBenchmarks(argv.provider)) {
+
+    //     const benchmarker = new Benchmarker(provider, serverBenchmarks);
+    //     const serverResults = await benchmarker.start({
+    //         id: serverId,
+    //         image: providerInfo.servers[serverId].image,
+    //         location: providerInfo.servers[serverId].location,
+    //         name: providerInfo.servers[serverId].name,
+    //         privateKey: providerInfo.settings.privateKey,
+    //         type: providerInfo.servers[serverId].type,
+    //         username: providerInfo.servers[serverId].username,
+    //     });
+
+    //     await storage.storeServerResults(serverId, serverResults);
+    // }
 })().then(() => {
     process.exit();
 }, (err) => {
