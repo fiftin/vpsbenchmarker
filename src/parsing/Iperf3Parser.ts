286a7f111fe7c42c0d9c09d175269b1622559b50
Denis <denguk@gmail.com>
2018-10-14 13:51:36 +0500

    feat: add iperf3 parser

---
+++ src/parsing/Iperf3Parser.ts
@@ -0,0 +1,33 @@
+
+export default class Iperf3Parser {
+    public parse(str: string): Map<string, any> {
+        const ret = new Map<string, any>();
+
+        const items = str.split("\n").map((line) => {
+            // [SUM]   7.00-8.00   sec  11.6 MBytes  97.0 Mbits/sec
+            const m = line.match(/^\[SUM]\s+[\d.-]+\s+sec\s+(?:[\d.]+) (?:\w+)\s+([\d.]+) (\w+)\/sec/);
+            if (!m) {
+                return;
+            }
+
+            const bandwidth = parseFloat(m[1]);
+
+            switch (m[2]) {
+                case "Kbits":
+                    return bandwidth * 1000;
+                case "Mbits":
+                    return bandwidth * 1000000;
+                case "Gbits":
+                    return bandwidth * 1000000000;
+            }
+
+            return bandwidth;
+        }).filter((item) => item != null);
+
+        ret.set("items", items);
+
+        ret.set("bandwidth", items.reduce((sum, value) => sum + value, 0) / items.length);
+
+        return ret;
+    }
+}
